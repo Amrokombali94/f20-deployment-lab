a392965c9b5f9831244aaf770bf107cc59053431
Amro Abdalla <wed.kom@hotmail.com>
2022-04-06 14:05:29 -0500

    intial commit

---
+++ server/index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+app.use(express.static('public'))
+
+app.get('/',function(req,res) {
+  res.sendFile(path.join(__dirname, '../index.html'));
+});
+
+const port = process.env.PORT || 4005
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`)
+})
